cd18ce6ec69ade983fd200dde4a10e16dc186f50
shita888 <sheetalnarwariya40@gmail.com>
2026-04-09 23:33:37 +0530

    new program

---
+++ Pattern-Printing/pyramid.c
@@ -0,0 +1,36 @@
+
+/*      * 
+       * * 
+      * * *  
+     * * * *  
+*/
+#include<stdio.h>
+void R_Tri(){
+int n;
+    printf("Enter num row: ");
+    scanf("%d",&n);
+
+    for(int i=1;i<=n;i++)
+    {
+        for(int k=1;k<=n-i;k++)
+        {
+          printf("  ",k);
+        } 
+       for(int j=1;j<=i;j++)
+        {
+          printf("* ",j);
+        } 
+
+         for(int j=1;j<=i-1;j++)
+        {
+          printf("* ",j);
+        } 
+        
+        printf("\n");
+    }
+    return;
+}
+ 
+int main(){
+    R_Tri(); 
+}
