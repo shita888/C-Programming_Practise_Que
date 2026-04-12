b20270b75f18e71f64e0634b3e19e6760cc222fb
shita888 <sheetalnarwariya40@gmail.com>
2026-04-12 23:59:46 +0530

    add program

---
+++ Pattern-Printing/Even_Square.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+
+    int n =4;
+   
+    for(int i=1;i<=n;i++)
+    {
+
+        for(int j=0;j<=2*n-1;j++)
+        { 
+            if(j%2==0){  
+                printf("%d ",j);
+            }
+        }   
+        printf("\n");
+    }
+
+ 
+      
+
+
+return 0;
+}
