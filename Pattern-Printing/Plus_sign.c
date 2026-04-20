d2c915fe95459db1962c9d5928703a038be4b24b
shita888 <sheetalnarwariya40@gmail.com>
2026-04-20 19:16:54 +0530

    new

--- Pattern-Printing/Plus_sign.c
+++ Pattern-Printing/Plus_sign.c
@@ -1,24 +1,31 @@
 #include <stdio.h>
 
 int main() {
-    int n=5;
+   int n;
+   printf("Enter the size of the plus sign (odd number): ");
+   scanf("%d", &n);
+
+
+if(n % 2 == 0) {
+   printf("Please enter an odd number.\n"); 
+}
+else{ 
+    int d = n/2 + 1;
    for(int i=1;i<=n;i++)
    { 
          for(int j=1;j<=n;j++)
          { 
-            //  if(i==1 && j==1 || i==1 && j==3 || i==3 && j==1 || i==3 && j==3){
-            //      printf("  ");
-            //      continue;
-            //  }
-            if(i==j || i==1 && j==n){
-                 printf("  ");
-                 continue;
-             }
-            printf("* "); 
-         }  
+            if(i==d || j==d)
+            {
+               printf("* ");
+            }
+            else{
+               printf("  ");
+               }
+         }
          printf("\n");
       
    }
-   
+} 
 return 0;
 }
