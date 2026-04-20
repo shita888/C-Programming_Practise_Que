8b096314eb438034e5b854fa8968ed5d4d27e990
shita888 <sheetalnarwariya40@gmail.com>
2026-04-20 19:17:41 +0530

    new

---
+++ Pattern-Printing/Cross_sign.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+int main() {
+   int n=3;
+//    printf("Enter the size of the plus sign (odd number): ");
+//    scanf("%d", &n);
+
+
+// if(n % 2 == 0) {
+//    printf("Please enter an odd number.\n"); 
+// }
+// else{ 
+    int d = n/2 + 1;
+   for(int i=1;i<=n;i++)
+   { 
+         for(int j=1;j<=n;j++)
+         { 
+            if( i==d || i!=j)
+            {
+               printf("# ");
+            }
+            else{
+               printf("* ");
+               }
+         }
+         printf("\n");
+      
+   }
+// } 
+return 0;
+}
